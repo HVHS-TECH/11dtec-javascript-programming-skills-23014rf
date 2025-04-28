2ff65c8d4571a7d77873889b882e8a6733dd304a
23014rf <23014rf@hvhs.school.nz>
2025-04-28 21:49:07 +0000

    Apr 28, 2025, 9:49 PM

--- t01_introduction.js
+++ t01_introduction.js
@@ -1,5 +1,3 @@
-<html>
-<style>
-<!--Roman Faulk 4/29/2025 I have made this to see improve on my coding skills over time.-->
 
-console.log("Running t01_introduction.js");
+
+console.log("Roman Faulk 4/29/2025 I have made this to see improve on my coding skills over time.");
