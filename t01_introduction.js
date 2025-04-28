fc231ec5f30dee63cd1d5141ddba41c6e1346e7d
23014rf <23014rf@hvhs.school.nz>
2025-04-28 21:54:56 +0000

    Apr 28, 2025, 9:54 PM

--- t01_introduction.js
+++ t01_introduction.js
@@ -1,3 +1,3 @@
 
 
-console.log("Roman Faulk 4/29/2025 I have made this to see improve on my coding skills over time.");
+console.log("Roman Faulk 4/29/2025 I have made this to see improvements on my coding skills over time.");
