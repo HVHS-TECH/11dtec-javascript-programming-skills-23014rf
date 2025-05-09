a18c4522038ee710c5a7b75088f03470db399053
23014rf <23014rf@hvhs.school.nz>
2025-05-09 03:00:16 +0000

    May 9, 2025, 3:00 AM

--- t02_variables.js
+++ t02_variables.js
@@ -20,7 +20,7 @@ let CurrentYear = "2025"
 
 /**********************/
 console.log("Hi", UserName,);
-console.log("As of", CurrentYear)
+console.log("As of", CurrentYear, "you are", UserAge,"Years old")
 console.log("You have ", PocketMoney, "dollars");
     // , As of 2025 you are 15 years old , you have 1 dollars");
 
