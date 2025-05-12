11383e319718a7a83aa8a381cb0a3e46093ab662
23014rf <23014rf@hvhs.school.nz>
2025-05-12 21:56:43 +0000

    changes

---
+++ t03_maths.js
@@ -0,0 +1,29 @@
+/**********************
+Task 3 Math
+Roman
+05/09/2025
+learning about maths
+**********************/
+
+console.log(  "Running T2-variables.js"  );
+
+//variables
+let UserName = "Roman Faulk";
+let UserAge = 16
+let PocketMoney = 1
+let CurrentYear = 2025
+var num1 = 10;
+var num2 = 2;
+var answer;
+/**********************
+ 
+//maincode
+
+/**********************/
+console.log("Hi", UserName,);
+console.log("As of", CurrentYear, "you are", UserAge,"Years old");
+console.log("You were born in",CurrentYear - UserAge );
+console.log("In 10 years you will be", num1 + UserAge,"years old");
+console.log("You have ", PocketMoney, "dollars");
+console.log("Half of your money is",PocketMoney / num2);
+/*********************/
