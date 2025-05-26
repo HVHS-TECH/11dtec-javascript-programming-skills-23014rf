76a11c9f6ad85e50acc908586f4befdd1126b92c
23014rf <23014rf@hvhs.school.nz>
2025-05-26 21:26:13 +0000

    May 26, 2025, 9:26 PM

---
+++ t02_variables.js
@@ -0,0 +1,23 @@
+/**********************
+Task 2 Vaiables
+Roman
+05/09/2025
+learning about variables
+**********************/
+
+console.log(  "running T2-variables.js"  );
+
+//variables
+let UserName = "Roman Faulk";
+let UserAge = 16
+let PocketMoney = 1
+let CurrentYear = 2025
+/**********************
+ 
+//maincode
+
+**********************/
+console.log("Hi", UserName,);
+console.log("As of", CurrentYear, "you are", UserAge,"Years old");
+console.log("You have ", PocketMoney, "dollars");
+/*********************/
---
+++ t05_alerts_prompts.js
@@ -0,0 +1,31 @@
+/**********************
+Task 5 alerts and prompts
+Roman
+05/09/2025
+learning about variables
+**********************/
+
+console.log(  "running T2-variables.js"  );
+
+//variables
+let UserName = "Roman Faulk";
+let UserAge = 16
+let PocketMoney = 1
+let CurrentYear = 2025
+alert 
+/**********************
+ 
+//maincode
+
+**********************/
+prompt("Whats your name \nEnter here");
+Alert("Hi", UserName,);
+prompt("How old are you \Enter her");
+Alert("you were born in", CurrentYear)
+prompt("How much moeney do you have \Enter here")
+Alert("You have", PocketMoney,"dollars")
+
+
+console.log("You have ", PocketMoney, "dollars");
+
+/*********************/
