4f1a8f7bc3fcbfbac87a4c2d02d7556091fc3fbf
23014rf <23014rf@hvhs.school.nz>
2025-06-04 00:21:07 +0000

    Jun 4, 2025, 12:21 AM

---
+++ t06_arrays.js
@@ -0,0 +1,31 @@
+/**********************
+Task 6 arrays
+Roman
+06/03/2025
+learning about Arrays
+**********************/
+
+console.log(  "running T2-variables.js"  );
+
+//variables
+let userName = "Roman Faulk";
+let userAge = 16
+let pocketMoney = 1
+let currentYear = 2025
+alert 
+/**********************/
+
+ let cars = ["Nissan", "Toyota", "Mazda", "Honda"]
+
+/**********************/
+Prompt("On a scale 0-3 how much do you like cars")
+prompt("Whats your name \nEnter here");
+Alert("Hi", UserName,);
+prompt("How old are you \Enter here");
+Alert("you were born in", CurrentYear)
+prompt("How much moeney do you have \Enter here")
+Alert("You have", PocketMoney,"dollars")
+
+console.log("You have ", PocketMoney, "dollars");
+
+/*********************/
---
+++ t07_if.js
@@ -0,0 +1,32 @@
+/**********************
+Task 7 arrays
+Roman
+06/03/2025
+learning about if and else
+**********************/
+
+console.log(  "running T2-variables.js"  );
+
+//variables
+let userName = "Roman Faulk";
+let userAge = 16
+let pocketMoney = 1
+let currentYear = 2025
+alert 
+/**********************/
+
+ let cars = ["Nissan", "Toyota", "Mazda", "Honda"]
+
+/**********************/
+Prompt("On a scale 0-3 how much do you like cars")
+
+prompt("Whats your name \nEnter here");
+
+Alert("Hi", UserName,);
+Alert("I predict you were born in 2009")
+Alert("Your pocketmoney is", userMoney "")
+Alert("Sorry you CAN'T afford a chocolate bar")
+
+roadhog, genji, bastion, ana, mercy
+
+/*********************/
