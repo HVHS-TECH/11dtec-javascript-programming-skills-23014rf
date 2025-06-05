778f2bb8176efa9ad1737f502c95a0b84923a7e1
23014rf <23014rf@hvhs.school.nz>
2025-06-05 01:07:11 +0000

    Jun 5, 2025, 1:07 AM

--- t08_for_loop.js
+++ t08_for_loop.js
@@ -1,29 +1,25 @@
-/**********************
-Task 8 arrays
-Roman
-06/03/2025
-learning about for loop
-**********************/
 
-console.log(  "running T2-variables.js"  );
+/***************
+Roman Faulk 
+
+Task 18
+
+Introduction to For loop
+***************/
+
+console.log(  "Running T1-intro.js"  );
 
 //variables
-let userName = "Roman Faulk";
-let userAge = 16
-let pocketMoney = 1
-let currentYear = 2025
-alert 
-/**********************/
 
- let cars = ["Nissan", "Toyota", "Mazda", "Honda"]
-/**********************/
-Prompt("On a scale 0-3 how much do you like cars")
 
-prompt("Whats your name \nEnter here");
+/**************/
 
-Alert("Hi", UserName,);
-Alert("I predict you were born in 2009")
-Alert("Your pocketmoney is", userMoney, "dollars")
-Alert("Sorry you CAN'T afford a chocolate bar")
+//mainCode
 
-/*********************/
+ /**************
+
+
+/*************
+ functions
+ *************/
+
