8f57ea83b02000085070bdbc299aa11fec253193
23014rf <23014rf@hvhs.school.nz>
2025-06-12 00:45:40 +0000

    Jun 12, 2025, 12:45 AM

--- t08_for_loop.js
+++ t08_for_loop.js
@@ -16,6 +16,22 @@ console.log(  "Running T1-intro.js"  );
 
 //mainCode
 
+
+
+//Loop from 1 to 5
+console.log("Starting Loop");
+for (count = 1; count< 5; count ++) {
+prompt("Pick a number");
+Prompt("pick a number");
+Prompt("Pcik a number");
+Prompt("Pcik a number");
+Prompt("Pcik a number");
+Alert("number")
+    console.log("Current Count: " + count);
+}
+
+Console/log("Loop Stopped!");
+
  /**************
 
 
